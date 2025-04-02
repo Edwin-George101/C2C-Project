bcbf1922f5b343f23bb8e21bd06bdd3b6528646c
Edwin-George101 <edwnslife@gmail.com>
2025-04-02 22:32:41 +0000

    hello message and menu

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 print("Hello User!")
-action = input("How are you doing? ")
-print(action)
+action = input("Welcome to the best bank in the world! We can check your balance, deposit funds, withdraw funds, create an account, delete an account, and lastly modify an account. What would you like to do? ")
+print("I would like to " + action)
 
